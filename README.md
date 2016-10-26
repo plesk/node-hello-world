ba9020757732f2ebfa2fd7e7050531002e734f68
Alexei Yuzhakov <sibprogrammer@gmail.com>
2016-10-26 21:47:38 +0700

    Initial import of the project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node.js Hello World App
+
+A simple app to test [Node.js](http://nodejs.org/) support.
---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end("Hello, World!\n");
+}).listen(process.env.PORT);
+
+console.log('App is running...');
